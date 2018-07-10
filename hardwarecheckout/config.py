59b0a9dcbbea586ba26eb68a2b561c72c1f9a7aa
yechs <chshu@protonmail.ch>
2018-07-10 10:53:58 +0800

    Content Customization Changed favicon.png to our owns Changed default item icon to our logo Changed html lang attribute Translated notices into Chinese (in config.py)

--- hardwarecheckout/config.py
+++ hardwarecheckout/config.py
@@ -16,31 +16,31 @@ QUILL_URL = os.environ['QUILL']
 SECRET = os.environ['SECRET']
 
 
-### The following variables may all be set using environment 
+### The following variables may all be set using environment
 ### variables of the same name. The environment variable will
-### take precedence over the value shown here. This is to allow 
-### for deploy configuration without modifying the codebase at all. 
-  
+### take precedence over the value shown here. This is to allow
+### for deploy configuration without modifying the codebase at all.
+
 ## Deploy settings ##
 
-# Enable/disable Flask debug mode. Should be set to 
-# False on production deploys of Cog. 
-DEBUG = True 
+# Enable/disable Flask debug mode. Should be set to
+# False on production deploys of Cog.
+DEBUG = True
 
-# Ensure that all attempts to visit an insecure version 
-# of the page redirect to  https, and makes sure all 
+# Ensure that all attempts to visit an insecure version
+# of the page redirect to  https, and makes sure all
 # Cog-derived redirects use https. Recommended to set
-# this to True if using an SSL-protected deploy. 
+# this to True if using an SSL-protected deploy.
 # Only works when DEBUG=False.
-FORCE_SSL = False 
+FORCE_SSL = False
 
-## Metadata ## 
+## Metadata ##
 
-HACKATHON_NAME = "HackMIT"
+HACKATHON_NAME = "Hack.init()"
 
 ## Event logistical settings ##
 
-# If true, lets users to make requests even if 
+# If true, lets users to make requests even if
 # item is out of stock
 ENABLE_WAITLIST = True
 
@@ -48,9 +48,9 @@ ENABLE_WAITLIST = True
 # for lotteried items
 LOTTERY_REQUIRES_PROPOSAL = True
 
-# If True, item type is set to CHECKOUT after an 
+# If True, item type is set to CHECKOUT after an
 # item's lottery is run
-CLOSE_LOTTERY_WHEN_RUN = False 
+CLOSE_LOTTERY_WHEN_RUN = False
 
 # If True, deny lottery requests that don't win
 # item
@@ -58,28 +58,23 @@ DENY_LOTTERY_LOSERS = True
 
 # Character limit for lottery proposals
 # Set to 0 to disable char limit
-LOTTERY_CHAR_LIMIT = 140 
+LOTTERY_CHAR_LIMIT = 140
 
 # If True, display current lottery item stock to user
-DISPLAY_LOTTERY_QUANTITY = True 
+DISPLAY_LOTTERY_QUANTITY = True
 
 # If True, display current checkout item stock to user
-DISPLAY_CHECKOUT_QUANTITY = True 
+DISPLAY_CHECKOUT_QUANTITY = True
 
 # If False, prevent users from submitting multiple
 # requests for same lottery item
-LOTTERY_MULTIPLE_SUBMISSIONS = False 
+LOTTERY_MULTIPLE_SUBMISSIONS = False
 
-# The info text underneath the 'Lottery Required' section 
-LOTTERY_TEXT = """We have a limited quantity of these items. Please fill out a 
-brief proposal describing your project idea, and we'll randomly accept as many 
-requests as we can 30 minutes after hacking starts."""
+# The info text underneath the 'Lottery Required' section
+LOTTERY_TEXT = """由于这些物品数量有限，需要通过抽签获取。请填写一份简要的计划，描述你在项目中使用这些物品的想法，我们将在Hackathon开始30分钟后随机通过尽可能多的请求。"""
 
-# The info text underneath the 'Checkout Required' section 
-CHECKOUT_TEXT = """Click to request any of these items, and your request will
-be approved when we have one available. Keep in mind we will ask to hold on
-to a form of ID until the item is returned."""
+# The info text underneath the 'Checkout Required' section
+CHECKOUT_TEXT = """点击以请求这些物品。你的请求将在物品可用时被通过。在物品被归还之前，我们将要求通过某种ID来记录物品状态。"""
 
-# The info text underneath the 'No Checkout Required' section 
-FREE_TEXT = """Just come to the hardware desk and ask for any of these
-items!"""
+# The info text underneath the 'No Checkout Required' section
+FREE_TEXT = """你可以直接到Hardware Desk请求以下物品！"""
